e353d7e1695c3250e110c97ff3ff7b6bf9df0c43
student-webdev-ph <mdaltafuddin456@gmail.com>
2026-03-22 19:22:13 +0600

    nav added

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+// Navbar.jsx
+
+import { Link } from "react-router";
 
 const Navbar = () => {
-    return (
-        <div>
-            This is nav
-        </div>
-    );
+  return (
+    <nav className="bg-white shadow-md px-6 py-4 flex justify-between items-center">
+      {/* Logo */}
+      <h1 className="text-xl font-bold text-indigo-600">Donate</h1>
+
+      {/* Links */}
+      <div className="space-x-6">
+        <Link to="/" className="hover:text-indigo-600">Home</Link>
+        <Link to="/donate" className="hover:text-indigo-600">Donate</Link>
+        <Link to="/about" className="hover:text-indigo-600">About</Link>
+        <Link to="/login" className="bg-indigo-600 text-white px-4 py-1 rounded hover:bg-indigo-700">
+          Login
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
